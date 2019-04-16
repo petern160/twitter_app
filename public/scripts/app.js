79f637a9fafec11e117ac77924f630e6923a65cf
petern160 <peternguyen160@gmail.com>
2019-04-16 10:57:48 -0700

    hover displays on tweet boxes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,19 +12,41 @@
 //     console.log(this); //The this keyword is a reference to the button
 //   });
 
+const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+  $(function(){
 
-
-$(function(){
-
+//   function renderTweets(tweets) {
+//     // loops through tweets
+//       // calls createTweetElement for each tweet
+//       // takes return value and appends it to the tweets container
+//   }
+  
+//   function createTweetElement(tweet) {
+//     let $tweet = $('<article>').addClass('tweet');
+//     // ...
+//     return $tweet;
+//   }
+  
+//  
     
-    var max_length = 140;
-    $('#inputcount').keyup(function (){
-        var textarea_length = $('#inputcount').val().length;
-        var maxTweet = max_length - textarea_length;
-        if(maxTweet < 0){
-            $('#count').css('color', 'red')
-        }
-        $('#count').html(maxTweet);
-    })
+    $.each(tweetData, function () {
+    $.each(this, function (name, value) {
+    console.log(name + '=' + value);
+    });
+    });
 
 });
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    
+    var max_length = 140;
+    $('#inputcount').keyup(function (){
+        var textarea_length = $('#inputcount').val().length;
+        var maxTweet = max_length - textarea_length;
+        if(maxTweet < 0){
+            $('#count').css('color', 'red')
+        }
+        $('#count').html(maxTweet);
+    })
+
+});
