138c80dc675db55d47d6cbf4905fbb2f47399f06
petern160 <peternguyen160@gmail.com>
2019-04-18 19:49:08 +0000

    fixed alignment on text counter and tweet button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,15 @@
-$(function(){
-
-    // char counter
-    var max_length = 140;
-    $('#inputcount').keyup(function (){
-        var textarea_length = $('#inputcount').val().length;
-        var maxTweet = max_length - textarea_length;
-        if(maxTweet < 0){
-            $('#count').css('color', 'red')
-        }
-        $('#count').html(maxTweet);
-    })
-
-});
+$(function () {
+  // char counter
+  var max_length = 140
+  $('#inputcount').keyup(function () {
+    var textarea_length = $('#inputcount').val().length
+    var maxTweet = max_length - textarea_length
+    console.log(textarea_length)
+    if (maxTweet < 0) {
+      $('#count').css('color', 'red')
+    } else {
+      $('#count').css('color', 'black')
+    }
+    $('#count').html(maxTweet)
+  })
+})
